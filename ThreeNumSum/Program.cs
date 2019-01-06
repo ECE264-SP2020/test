644d89696921c1790f3eff61da8792ecdd5b378d
bveina <bveina@users.noreply.github.com>
2019-01-06 12:31:36 -0500

    added travis File

--- ThreeNumSum/Program.cs
+++ ThreeNumSum/Program.cs
@@ -2,11 +2,50 @@
 
 namespace ThreeNumSum
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int? a, b, c;
+            a = getNumNull();
+            b = getNumNull();
+            c = getNumNull();
+            if (a != null && b != null && c != null)
+            {
+                Console.WriteLine("{0}", a + b + c);
+            }
+            else
+            {
+                Console.WriteLine("Bad Input");
+            }
+        }
+        static int getNum()
+        {
+            int val;
+            string myInput;
+            myInput = Console.ReadLine();
+            bool res = int.TryParse(myInput, out val);
+            while (!res)
+            {
+                myInput = Console.ReadLine();
+                res = int.TryParse(myInput, out val);
+            }
+            return val;
+        }
+        static int? getNumNull()
+        {
+            int val;
+            string myInput;
+            myInput = Console.ReadLine();
+            bool res = int.TryParse(myInput, out val);
+            if (res)
+            {
+                return val;
+            }
+            else
+            {
+                return null;
+            }
         }
     }
 }
---
+++ ThreeNumSumTests/UnitTest1.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+
+
+namespace ThreeNumSumTests
+{
+    public class MyFixture :IDisposable
+    {
+        public int score;
+        //private readonly ITestOutputHelper output;
+        public ITestOutputHelper output;
+        public MyFixture()
+        {
+            score = new int();
+            //this.output = output;
+        }
+
+        public void Dispose()
+        {
+            output.WriteLine("Score: {0}", score);
+            //Debug.WriteLine("Dispose Fixture called");
+        }
+    }
+
+    public class UnitTest1: IClassFixture<MyFixture>
+    {
+        private MyFixture _fixture;
+
+        private readonly ITestOutputHelper output;
+        public UnitTest1(MyFixture fixture ,ITestOutputHelper output)
+        {
+            _fixture = fixture;
+            this.output = output;
+            _fixture.output = output;
+            //score = 0;
+        }
+        
+        [Fact]
+        public void Test1()
+        {
+            // setup the output
+            var content = new StringBuilder();
+            var writer = new StringWriter(content);
+            Console.SetOut(writer);
+
+            var reader = new StringReader("5\n4\n7\n");
+            Console.SetIn(reader);
+            ThreeNumSum.Program.Main(null);
+            Assert.Equal("16\r\n", content.ToString());
+            _fixture.score += 50;
+            output.WriteLine("Score: {0}", _fixture.score);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            // setup the output
+            var content = new StringBuilder();
+            var writer = new StringWriter(content);
+            Console.SetOut(writer);
+
+            var reader = new StringReader("bob\n4\n7\n");
+            Console.SetIn(reader);
+            ThreeNumSum.Program.Main(null);
+            Assert.Equal("Bad Input\r\n", content.ToString());
+            _fixture.score += 50;
+            output.WriteLine("Score: {0}", _fixture.score);
+        }
+        [Fact]
+        public void Test3()
+        {
+            output.WriteLine("Score: {0}", _fixture.score);
+        }
+    }
+}
